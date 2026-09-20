490b242b4dae6e97a23d1cb8e3e7775e1ae43017
Tom Manning <tom@manningetal.com>
2026-09-20 07:11:16 -0400

    add Table_def_delivery_2column

--- defines.py
+++ defines.py
@@ -17,15 +17,23 @@ AM_PM_TYPE = 'AM_PM'  # used for AM/PM guest lists
 class Table_def_pickup_by_name:
    number_of_columns = 2
    number_of_rows_on_a_page = 40
-   header = ["Time", "First", "Last", "Items"]
+   header = ["First", "Last", "Time", "Items"]
    column_widths = [64, 90, 54, 36]
    column_font = [12, 12, 12, 12]
    center_spacer_width = 50
 
 class Table_def_delivery_expeditor:
    number_of_columns = 1
-   number_of_rows_on_a_page = 36
+   number_of_rows_on_a_page = 39
    header = ["Bag", "Bags","Route", "First", "Last", "Items", "Phone"]
-   column_widths = [30, 36, 200, 72, 84, 36, 90]
+   column_widths = [30, 34, 190, 72, 84, 36, 90]
    column_font = [14, 14, 14, 14, 14, 14, 14]
    center_spacer_width = 0 # unused
+
+class Table_def_delivery_2column:
+   number_of_columns = 2
+   number_of_rows_on_a_page = 38
+   header = ["First", "Last", "Route", "Items"]
+   column_widths = [64, 90, 54, 36]
+   column_font = [12, 12, 12, 12]
+   center_spacer_width = 40
